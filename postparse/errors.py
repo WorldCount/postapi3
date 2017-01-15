cf7e36184581f94a148bf6bf6f5e97c20396e6ab
WorldCount <world.count@yandex.ru>
2017-01-15 19:34:13 +0300

    Написаны классы ошибок. Мелкий рефакторинг.

--- postparse/errors.py
+++ postparse/errors.py
@@ -11,10 +11,10 @@ __author__ = 'WorldCount'
 """
 
 
-#
+# Общий класс для ошибок почтовых файлов
 class PostError(Exception):
 
-    def __init__(self, link='', more='', show_full_link=False):
+    def __init__(self, link=None, more=None, show_full_link=False):
         self.link = link
         self.dir_file = ''
 
@@ -25,5 +25,88 @@ class PostError(Exception):
         self.more = more
 
     def error_message(self):
-        return '%s: %s' % (self.msg, self.link)
+        if self.more:
+            return '%s: %s, сообщение: %s' % (self.msg, self.link, str(self.more))
+        else:
+            return '%s: %s' % (self.msg, self.link)
 
+
+# Ошибка парсинга имени почтового файла
+class ParseNameError(PostError):
+
+    def __init__(self, link=None, more=None, show_full_link=False):
+        super(ParseNameError, self).__init__(link, more, show_full_link)
+        self.msg = 'Ошибка парсинга имени файла'
+
+
+# ОШИБКИ ФАЙЛА
+# Ошибка при работе с почтовым файлом
+class PostFileError(PostError):
+
+    def __init__(self, link=None, more=None, show_full_link=False):
+        super(PostFileError, self).__init__(link, more, show_full_link)
+        self.msg = 'Ошибка при работе с почтовым файлом'
+        self.more = more
+
+
+# Ошибка парсинга почтового файла. Файл пуст.
+class EmptyPostFile(PostFileError):
+
+    def __init__(self, link=None, more=None, show_full_link=False):
+        super(EmptyPostFile, self).__init__(link, more, show_full_link)
+        self.msg = 'Ошибка, файл пустой'
+
+
+# Ошибка парсинга почтового файла. Не найден файл в архиве.
+class FileInZipNotFound(PostFileError):
+
+    def __init__(self, link=None, more=None, show_full_link=False):
+        super(FileInZipNotFound, self).__init__(link, more, show_full_link)
+        self.msg = 'Не найдено вложение в файле'
+
+
+# Ошибка парсинга почтового файла. В файле больше 1 вложения.
+class AttachInZipError(PostFileError):
+
+    def __init__(self, link=None, more=None, show_full_link=False):
+        super(AttachInZipError, self).__init__(link, more, show_full_link)
+        self.msg = 'Больше одного вложения в архиве'
+
+
+# Ошибка парсинга почтового файла. Ошибка типа почтового файла
+class TypeFileError(PostFileError):
+
+    def __init__(self, link=None, more=None, show_full_link=False):
+        super(TypeFileError, self).__init__(link, more, show_full_link)
+        self.msg = 'Неизвестный формат файла'
+
+
+# ОШИБКИ ПАРСИНГА
+# Ошибка парсинга почтового файла
+class PostParseError(PostError):
+
+    def __init__(self, link=None, more=None, show_full_link=False):
+        super(PostParseError, self).__init__(link, more, show_full_link)
+        self.msg = 'Ошибка парсинга файла'
+
+    def error_message(self):
+        if self.more:
+            return '%s: %s, строка: %s' % (self.msg, self.link, str(self.more))
+        else:
+            return '%s: %s' % (self.msg, self.link)
+
+
+# Ошибка парсинга строки почтового файла
+class ParseStringError(PostParseError):
+
+    def __init__(self, link=None, more=None, show_full_link=False):
+        super(ParseStringError, self).__init__(link, more, show_full_link)
+        self.msg = 'Ошибка парсинга строки'
+
+
+# Ошибка парсинга почтового файла. Не найден номер РПО
+class RpoNumNotFound(PostParseError):
+
+    def __init__(self, link=None, more=None, show_full_link=False):
+        super(RpoNumNotFound, self).__init__(link, more, show_full_link)
+        self.msg = 'Не найден номер РПО'
---
+++ postparse/packfile.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'WorldCount'
+
+
+from . import postfile, errors
